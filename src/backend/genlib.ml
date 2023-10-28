3549264ba9bf90bbe96f03e56dae22a56293a48b
mikhaylovilya <ily2407@gmail.com>
2023-10-28 15:11:36 +0300

    Implement simple interface in compiler.ml and in genlib.ml
    Also provide nesessary deps in dune files

--- src/backend/genlib.ml
+++ src/backend/genlib.ml
@@ -1,16 +1,19 @@
-open Core
-
 (* open Core_unix *)
 (* open Graph.Persistent.Graph *)
 
-type t = Ast.myfile
+open Ast
+
+type t = myfile
 
 (* let embox_root = "/home/cy/Desktop/ocaml-rep/embox" *)
+(* TODO: make pure *)
 let embox_root = "/home/cy/Desktop/ocaml-rep/caravan"
 
 let gen_package package =
   let _ = package in
-  let _ = String.concat ~sep:"/" package |> Core_unix.mkdir_p in
+  let cat = Core.String.concat ~sep:"/" (embox_root :: package) in
+  (* let _ = Core.Printf.printf "%s\n" cat in *)
+  let _ = Core_unix.mkdir_p cat in
   Result.Ok ()
 ;;
 
@@ -24,10 +27,15 @@ let gen_entities entities =
   Result.Ok ()
 ;;
 
-let gen trees =
-  List.iter
+let gen (trees : t list) =
+  Core.List.iter
     ~f:(fun (package, imports, entities) ->
-      match gen_package package, gen_imports imports, gen_entities entities with
+      let unwrap_package =
+        match package with
+        | Some x -> x
+        | None -> []
+      in
+      match gen_package unwrap_package, gen_imports imports, gen_entities entities with
       | Result.Ok (), Ok (), Ok () -> ()
       | _ -> ())
     trees
--- src/compiler.ml
+++ src/compiler.ml
@@ -1,10 +1,23 @@
 (* let () = print_endline "Hello, World!" *)
 
-(* type config = { embox_root : string }
-
-let parse_args () =
-  let open Core in
-  Command.basic_spec
+(* type config = { embox_root : string } *)
+let compile ~input ~filename =
+  match Parselib.parse_with_err ~input ~filename with
+  | Some value -> Genlib.gen [ value ]
+  | None -> ()
 ;;
 
-let () = () *)
+let loop filename () =
+  let open Core in
+  let ic = In_channel.create filename in
+  let _ = compile ~input:(In_channel.read_all filename) ~filename in
+  In_channel.close ic
+;;
+
+let () =
+  Core.Command.basic_spec
+    ~summary:"Compile myfile"
+    Command.Spec.(empty +> anon ("filename" %: string))
+    loop
+  |> Command_unix.run
+;;
